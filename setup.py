5f6165329ce626b7cdf87ffb70231d25f502a738
Alejandro Otero Ortiz de Cosca <aoterort@gmail.com>
2015-12-30 17:21:28 +0100

    Changed long description

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ PACKAGES = ['redsys', ]
 
 setup(
         name='redsys-256',
-        description='This package can generate the necessary fields to the Redsys payment form.',
-        keywords='redsys hmac sha256',
+        description='Generates Redsys form parameters with new hmac sha256 encryption protocol.',
+        long_description='This package allows to generate the appropriate parameters for new Redsys encryption protocol HMAC SHA256.',
+        keywords='redsys hmac sha256 migration 256 python',
         author='Alejandro Otero Ortiz de Cosca',
         author_email='otero.alx@gmail.com',
         url='http://www.aotero.es',
         download_url="https://github.com/lexotero/python-redsys",
-        version='0.1.1',
+        version='0.1.2',
         license='MIT',
         provides=[
             'redsys'
