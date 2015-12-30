7f4245f06ea9a4ca69db7945cd5e64a2c7098c5b
Alejandro Otero Ortiz de Cosca <aotero@zertifica.com>
2015-12-30 10:53:14 +0100

    updated license and setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,19 @@ from distutils.core import setup
 PACKAGES = ['redsys', ]
 
 setup(
-        name='redsys',
-        description='Creates and encrypt the form parameter for RedSys Payment',
+        name='redsys-256',
+        description='This package can generate the necessary fields to the Redsys payment form.',
         author='Alejandro Otero Ortiz de Cosca',
         author_email='otero.alx@gmail.com',
-        url='http://aotero.es',
+        url='http://www.aotero.es',
         download_url="https://github.com/lexotero/python-redsys",
-        version='0.0.1',
-        license='MIT License',
+        version='0.1.0',
+        license='MIT',
         provides=[
             'redsys'
         ],
         install_requires=[
-            'Crypto',
-            'pycrypto'
+            'Crypto'
         ],
         packages=PACKAGES,
         scripts=[],
